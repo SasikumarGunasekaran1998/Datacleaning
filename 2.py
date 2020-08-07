7a62a8dc7fa33285fd1f5af3d810ffbf0f096cf3
SasikumarGunasekaran1998 <50619565+SasikumarGunasekaran1998@users.noreply.github.com>
2020-08-07 15:24:12 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,7 +0,0 @@
-subjects=["Americans","Indians"]
-verbs=["play","watch"]
-objects=["Baseball","Cricket"]
-for i in subjects:
-    for j in verbs:
-        for k in objects:
-            print(i+" "+j+" "+k)
